c65ae63f5f09ac243b72f8d86ea75ebbcea4649a
Jesús Quintana <jquintana@atlassian.com>
2019-11-07 15:36:28 +1100

    Using existing testing file

--- test/typescript/prosemirror-tables.ts
+++ test/typescript/prosemirror-tables.ts
@@ -1,9 +1,23 @@
-import * as tables from '../../';
+import {TableMap, toggleHeader, TableRect, tableEditing} from '../../';
+import {Node as ProsemirrorNode} from "prosemirror-model";
 
-const table = tables.tableEditing();
-const tableWithNodeSelection = tables.tableEditing({allowTableNodeSelection: true});
+const tableEditing1 = tableEditing();
+const tableWithNodeSelection = tableEditing({allowTableNodeSelection: true});
 
-tables.toggleHeader('column');
-tables.toggleHeader('row');
-tables.toggleHeader('row', { useDeprecatedLogic: false });
-tables.toggleHeader('row', { useDeprecatedLogic: true });
+const map = new TableMap();
+const table = new ProsemirrorNode();
+
+toggleHeader('column');
+toggleHeader('row');
+toggleHeader('row', { useDeprecatedLogic: false });
+toggleHeader('row', { useDeprecatedLogic: true });
+
+const tableRect: TableRect = {
+    left: 10,
+    top: 20,
+    right: 30,
+    bottom: 40,
+    tableStart: 20,
+    map,
+    table,
+};
