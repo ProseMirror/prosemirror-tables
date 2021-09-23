e80e3440a3744850c7ff41cbc920a0efdef2d6ad
Ofir Simhi <ofirsimhi1612@gmail.com>
2021-09-23 16:37:28 +0300

    remove stats.html

--- rollup.config.js
+++ rollup.config.js
@@ -4,7 +4,6 @@ import external from 'rollup-plugin-peer-deps-external';
 import postcss from 'rollup-plugin-postcss';
 import resolve from '@rollup/plugin-node-resolve';
 import image from '@rollup/plugin-image';
-import visualizer from 'rollup-plugin-visualizer';
 import pkg from './package.json';
 
 export default {
@@ -30,6 +29,5 @@ export default {
       include: 'node_modules/**',
     }),
     image(),
-    visualizer(),
   ],
 };
