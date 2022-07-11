d6c1ce4826e697efe89ff772a2a0ddea484c661d
ocavue <ocavue@users.noreply.github.com>
2022-07-11 21:52:02 +0800

    Bundle package with ES2015 spec (#172)

--- rollup.config.js
+++ rollup.config.js
@@ -1,20 +1,17 @@
-import buble from '@rollup/plugin-buble';
-
 export default {
   input: './src/index.js',
   output: [
     {
-      file: 'dist/index.js',
+      file: 'dist/index.cjs',
       format: 'cjs',
       sourcemap: true,
     },
     {
-      file: 'dist/index.es.js',
+      file: 'dist/index.js',
       format: 'es',
       sourcemap: true,
     },
   ],
-  plugins: [buble()],
   external(id) {
     return !/^[\.\/]/.test(id);
   },
