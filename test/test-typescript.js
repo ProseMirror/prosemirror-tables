ff7fd0e5000228da9086caa19b70af0e711f4b40
MH4GF <h.miyagi.cnw@gmail.com>
2022-07-20 16:55:34 +0900

    fix typings test is not worked
    Currently, errors of the type that should fail are also green.
    
    https://github.com/ProseMirror/prosemirror-tables/commit/49acc2dbc3d1bd030e885dc6615ae461e665add6
    The result of `path.resolve()` changed here PR was the root directory, because of the directory where the node process runs.
    By using import.meta.url, the correct path can be specified independent of the execution location.

--- test/test-typescript.js
+++ test/test-typescript.js
@@ -1,9 +1,11 @@
 import { checkDirectory } from 'typings-tester';
 import path from 'path';
+import { fileURLToPath } from 'url';
 
 describe('TypeScript definitions', function () {
   it('should compile against index.d.ts', () => {
-    const __dirname = path.resolve();
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
     checkDirectory(__dirname + '/typescript');
   });
 });
