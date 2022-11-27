06a56b03a4582aa421abefe6de19b1a2b949585f
ocavue <ocavue@users.noreply.github.com>
2022-11-28 00:27:27 +0800

    Use tsup to bundle the library (#189)

---
+++ tsup.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from 'tsup';
+
+export default defineConfig({
+  entry: ['src/index.ts'],
+  format: ['esm', 'cjs'],
+  dts: true,
+  splitting: false,
+  sourcemap: false,
+  clean: true,
+});
