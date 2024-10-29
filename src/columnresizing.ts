3c09ce38f139863dac4ca1211f52e61955153cfa
yousefed <yousefdardiry@gmail.com>
2024-10-29 10:28:35 +0100

    fix defaultCellMinWidth handling for safari

--- src/columnresizing.ts
+++ src/columnresizing.ts
@@ -234,6 +234,8 @@ function handleMouseDown(
     }
   }
 
+  displayColumnWidth(view, pluginState.activeHandle, width, defaultCellMinWidth);
+
   win.addEventListener('mouseup', finish);
   win.addEventListener('mousemove', move);
   event.preventDefault();
@@ -397,6 +399,12 @@ export function handleDecorations(
       const pos = start + cellPos + table.nodeAt(cellPos)!.nodeSize - 1;
       const dom = document.createElement('div');
       dom.className = 'column-resize-handle';
+      if (columnResizingPluginKey.getState(state)?.dragging) {
+        decorations.push(Decoration.node(start + cellPos, start + cellPos + table.nodeAt(cellPos)!.nodeSize, {
+          class: 'column-resize-dragging'
+        }));
+      }
+      
       decorations.push(Decoration.widget(pos, dom));
     }
   }
