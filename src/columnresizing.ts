90c250d69fd0debc30b4d3a5e0955c1c6f8d230e
Dominik Biedebach <dominik@bdbch.com>
2025-01-29 16:41:20 +0100

    fix: don't handle mouse events for resize plugin when editor is not editable

--- src/columnresizing.ts
+++ src/columnresizing.ts
@@ -142,6 +142,9 @@ function handleMouseMove(
   handleWidth: number,
   lastColumnResizable: boolean,
 ): void {
+  // check if editor is editable first before handling any events
+  if (!view.editable) return;
+
   const pluginState = columnResizingPluginKey.getState(view.state);
   if (!pluginState) return;
 
@@ -178,6 +181,8 @@ function handleMouseMove(
 }
 
 function handleMouseLeave(view: EditorView): void {
+  if (!view.editable) return;
+
   const pluginState = columnResizingPluginKey.getState(view.state);
   if (pluginState && pluginState.activeHandle > -1 && !pluginState.dragging)
     updateHandle(view, -1);
@@ -189,6 +194,8 @@ function handleMouseDown(
   cellMinWidth: number,
   defaultCellMinWidth: number,
 ): boolean {
+  if (!view.editable) return false;
+
   const win = view.dom.ownerDocument.defaultView ?? window;
 
   const pluginState = columnResizingPluginKey.getState(view.state);
