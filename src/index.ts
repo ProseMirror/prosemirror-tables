0881feb3cf2f121a2260dc87cdb64cb11cf7b971
ocavue <ocavue@users.noreply.github.com>
2025-04-14 13:10:57 +1000

    docs: add inline comment (#284)

--- src/index.ts
+++ src/index.ts
@@ -67,6 +67,15 @@ export { tableEditingKey };
  * @public
  */
 export type TableEditingOptions = {
+  /**
+   * Whether to allow table node selection.
+   *
+   * By default, any node selection wrapping a table will be converted into a
+   * CellSelection wrapping all cells in the table. You can pass `true` to allow
+   * the selection to remain a NodeSelection.
+   *
+   * @default false
+   */
   allowTableNodeSelection?: boolean;
 };
 
