7cfad17335ab8da3913da2284a4137bb24942d46
ocavue <ocavue@gmail.com>
2025-12-04 05:48:27 +1100

    ignore some rules

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,10 @@
 import { defineESLintConfig } from '@ocavue/eslint-config';
 
-export default defineESLintConfig();
+export default defineESLintConfig({}, [
+    {
+        rules: {
+            "unicorn/prefer-math-trunc": "off",
+            "unicorn/no-for-loop": 'off'
+        }
+    }
+]);
